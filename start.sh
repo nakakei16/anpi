2c1e1ff5ea457361036b0f04cd039043fe4dfa00
e11808 <e11808@PM6IKSA02451A.kmjp.local>
2017-07-11 18:34:03 +0900

    change encode to UTF-8

--- start.sh
+++ start.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-CLASSPATH=$CLASSPATH:bin/:lib/pi4j-core.jar:lib/pi4j-device.jar:lib/pi4j-example.jar:lib/pi4j-gpio-extension.jar:lib/pi4j-service.jar
+CLASSPATH=$CLASSPATH:bin/:lib/pi4j-core.jar:lib/pi4j-device.jar:lib/pi4j-example
+.jar:lib/pi4j-gpio-extension.jar:lib/pi4j-service.jar
 export CLASSPATH
 
-javac src/com/naka/anpi/AnpiMain.java -d bin/com/naka/anpi/
+
+mkdir bin
+
+javac src/com/naka/anpi/AnpiMain.java -d bin
 java com.naka.anpi.AnpiMain
 
