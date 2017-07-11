f9f17f2c83e59d271d22caa9459a4a60bbde4206
e11808 <e11808@PM6IKSA02451A.kmjp.local>
2017-07-11 11:35:16 +0900

    fit to raspi env

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CLASSPATH=$CLASSPATH:pi4j-core.jar:pi4j-device.jar:pi4j-example.jar:pi4j-gpio-extension.jar:pi4j-service.jar
+CLASSPATH=$CLASSPATH:bin/:lib/pi4j-core.jar:lib/pi4j-device.jar:lib/pi4j-example.jar:lib/pi4j-gpio-extension.jar:lib/pi4j-service.jar
 export CLASSPATH
 
 java com.naka.anpi.AnpiMain
