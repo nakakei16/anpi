3dcc3eb3f3a7173781693ef72bda4c22d30c4346
e11808 <e11808@PM6IKSA02451A.kmjp.local>
2017-07-12 16:43:24 +0900

    debug

--- src/com/naka/anpi/AnpiMain.java
+++ src/com/naka/anpi/AnpiMain.java
@@ -27,7 +27,8 @@ public class AnpiMain {
 			System.out.println("Anpi Start!!");
 
 			for (int i = 0; i < 100; i++) {
-				anpi.flash();
+//				anpi.flash();
+				anpi.check_flas();
 				Thread.sleep(1000);
 			}
 
