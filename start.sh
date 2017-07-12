56d5c9c4b9ed8487c006b81cd87d9890e5abfbc2
e11808 <e11808@PM6IKSA02451A.kmjp.local>
2017-07-12 10:45:28 +0900

    change next line code

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-CLASSPATH=$CLASSPATH:bin/:lib/pi4j-core.jar:lib/pi4j-device.jar:lib/pi4j-example
-.jar:lib/pi4j-gpio-extension.jar:lib/pi4j-service.jar
+CLASSPATH=$CLASSPATH:bin/:lib/pi4j-core.jar:lib/pi4j-device.jar:lib/pi4j-example.jar:lib/pi4j-gpio-extension.jar:lib/pi4j-service.jar
 export CLASSPATH
 
 
